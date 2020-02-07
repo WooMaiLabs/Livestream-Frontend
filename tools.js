cdf82019cc75e3edab9cff69a5b1116114d7d1dd
WooMai <allentian666@gmail.com>
2020-02-08 04:19:07 +0800

    add custom endpoint support

--- tools.js
+++ tools.js
@@ -12,3 +12,14 @@ function getQueryVariable(variable) {
     return (false);
 }
 
+/**
+ * @link https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url 
+ */
+function isEndpoint(str) {
+    var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+        '?$', 'i');
+    return !!pattern.test(str);
+}
