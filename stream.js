70673c045c8d63ee4a079fe92bef1182994e7fab
WooMai <allentian666@gmail.com>
2020-02-11 00:05:50 +0800

    add ajax api support

---
+++ stream.js
@@ -0,0 +1,60 @@
+$(function () {
+    init();
+});
+
+function init() {
+    $.ajax({
+        url: 'https://live.wmapi.net/v1/info/stream/' + getQueryVariable('stream'),
+        dataType: 'json',
+        success: function (result) {
+            console.log(result);
+            if (result.ret != 0) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops!',
+                    text: ret.msg
+                });
+                OSPF();
+            } else {
+                if (result.data.status == 'ended') {
+                    Swal.fire({
+                        icon: 'info',
+                        title: '来晚了',
+                        text: '该直播已结束'
+                    });
+                    OSPF();
+                } else {
+                    $('#subtitle').text('Streamer: ' + result.data.streamer);
+
+                    url = result.data.play_url;
+                    mirror = getQueryVariable('mirror');
+                    if (mirror != false && isEndpoint(mirror)) {
+                        url = mirror + getQueryVariable('stream') + '.m3u8';
+                    }
+
+                    const dp = new DPlayer({
+                        container: document.getElementById('dplayer'),
+                        live: true,
+                        autoplay: true,
+                        video: {
+                            url: url
+                        }
+                    });
+                }
+            }
+        },
+        complete: function (XHR, TS) { XHR = null }
+    });
+}
+
+function OSPF()
+{
+    const dp = new DPlayer({
+        container: document.getElementById('dplayer'),
+        live: true,
+        autoplay: true,
+        video: {
+            url: 'OSPF.mp4'
+        }
+    });
+}
